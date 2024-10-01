a7afe775796247bbd449066316582d08966d9b5d
mentallyununstable <jonasans@icloud.com>
2024-10-01 10:19:10 +0200

    Created RaffleCup

--- game/RaffleCup.java
+++ game/RaffleCup.java
@@ -1,5 +1,21 @@
 package game;
 
 public class RaffleCup {
-    
+    private Die[] dice;
+
+    public RaffleCup() {
+        dice = new Die[2];
+        dice[0] = new Die(); 
+        dice[1] = new Die();
+    }
+
+    public int getSum() {
+        dice[0].roll();
+        dice[1].roll();
+        return dice[0].getFaceValue() + dice[1].getFaceValue();
+    }
+
+    public boolean isSame() {
+        return dice[0].getFaceValue() == dice[1].getFaceValue();
+    }
 }
