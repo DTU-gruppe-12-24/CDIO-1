6b7cf48c36d020a7564f2c2e45cd814574a64c5d
Stevnbak <kasper@stevnbak.dk>
2024-10-03 13:55:37 +0200

    Document DiceGame class

--- game/main.java
+++ game/main.java
@@ -1,17 +1,31 @@
 package game;
 
+/** 
+ * Class for the entry point of the program.
+ */
 class Program {
+    /** 
+     * Main function of the program.
+     * Creates and starts the game.
+     */
     public static void main(String[] args) {
         DiceGame game = new DiceGame();
-        game.main();
+        game.Play();
     }
 }
 
+/** 
+ * Class for the main game flow and connecting every part of the project.
+ */
 class DiceGame {
     private Player[] players;
     private RaffleCup raffleCup;
     private java.util.Scanner scanner;
 
+    /**
+     * Constructor of the DiceGame class.
+     * We create the input scanner, player and rafflecup objects.
+     */
     public DiceGame() {
         scanner = new java.util.Scanner(System.in);
 
@@ -22,29 +36,35 @@ class DiceGame {
         raffleCup = new RaffleCup();
     }
 
-    public void main() {
-        //reset score
+    /**
+     * Starts and plays through the entire game loop.
+     */
+    public void Play() {
+        // Reset the score of every player, in case the game was restarted.
         for(var player : players) player.setScore(0);
-        //2. Select starting player
+
+        // Usecase 1 step 2: Randomly selecting the starting player
         int currentPlayerIndex = (int) (Math.random() * 2);
         Player currentPlayer = players[currentPlayerIndex];
-        //Player turn
+
+        // Game loop (one run through the loop is a single player turn)
         boolean playing = true;
         while(playing) {
-            //3. Roll the dice
+            // Usecase 1 step 3: Rolling the dice.
             System.out.println("Player " + currentPlayer.getId() + " press enter to roll the dice.");
             scanner.nextLine();
             raffleCup.roll();
-            //4. Feedback
+            // Usecase 1 step 4: Displaying feedback from dice rolls.
             int sum = raffleCup.getSum();
             int value1 = raffleCup.getValue(0);
             int value2 = raffleCup.getValue(1);
             System.out.println("Die 1 value: " + value1);
             System.out.println("Die 2 value: " + value2);
             System.out.println("Total value: " + sum);
-            //5. Rules
+
+            // Usecase 1 step 5: Checking the dice and updating the game state according to the game rules.
             boolean playerWon = false;
-            //  ii.
+            // Usecase 1 step 5.ii: Winning by rolling two 6's twice in a row.
             if(value1 == 6 && value2 == 6) {
                 if(currentPlayer.getLastWasSixes()) {
                     playerWon = true;
@@ -54,44 +74,54 @@ class DiceGame {
             } else {
                 currentPlayer.setLastWasSixes(false);
             }
-            //  iii.
+            //   Usecase 1 step 5.iii: Winning by rolling two of the same after reaching 40 points.
             if(currentPlayer.getScore() >= 40 && raffleCup.isSame() && !(value1 == 1 && value2 == 1)) {
                 playerWon = true;
                 System.out.println("Player " + currentPlayer.getId() + " won the game, by rolling two of the same after reaching 40 points!");
             }
-            //  i. update score
+            //   Usecase 1 step 5.i: Updating the score, and resetting to 0 by rolling two 1's:
             if(value1 == 1 && value2 == 1) {
                 currentPlayer.setScore(0);
             } else {
                 currentPlayer.setScore(currentPlayer.getScore() + sum);
             }
-            //6. Feedback
+
+            // Usecase 1 step 6: Displaying the updated game state.
             System.out.println("Player 1 score: " + players[0].getScore());
             System.out.println("Player 2 score: " + players[1].getScore());
             if(playerWon) System.out.println("Player " + currentPlayer.getId() + " won the game!");
-            //Continue
+            
+            // If the player won, end the game
             if(playerWon) {
                 playing = false;
-            } else {
-                //  5.iv.
+            }
+            // Pick the next player and repeat loop
+            else {
+                //  Usecase 1 step 5.iv: Player gets another turn by rolling two of the same
                 if(raffleCup.isSame() == false) {
+                    // Change next player
                     if(currentPlayerIndex == 0) currentPlayerIndex = 1;
                     else currentPlayerIndex = 0;
                     currentPlayer = players[currentPlayerIndex];
                 }
             }
         }
-        //7. Restart?
+        // Usecase 1 step 7: Game ended. Let players choose to restart or end program.
         boolean decided = false;
         while(!decided) {
             System.out.println("Do you want to play again? (yes/no)");
             String answer = scanner.nextLine().toLowerCase();
+            //Play game again
             if(answer.equals("yes")) {
                 decided = true;
-                main();
-            } else if(answer.equals("no")) {
+                Play();
+            }
+            //Leave function
+            else if(answer.equals("no")) {
                 decided = true;
-            } else {
+            }
+            //Ask again
+            else {
                 System.out.println("Unrecognized answer");
             }
         }
