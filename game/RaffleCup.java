4cf69ba442af88c5c7ac4e05fc44fef74c022ea6
jonashersom <97104944+jonashersom@users.noreply.github.com>
2024-10-07 10:37:00 +0200

    Added coments for Rafflecup.java

--- game/RaffleCup.java
+++ game/RaffleCup.java
@@ -1,28 +1,34 @@
 package game;
 
 public class RaffleCup {
+    // Array to store two dice
     private Die[] dice;
 
+    // Constructor initializes the array with two new Die objects
     public RaffleCup() {
-        dice = new Die[2];
-        dice[0] = new Die(); 
-        dice[1] = new Die();
+        dice = new Die[2];   // Create an array of two dice
+        dice[0] = new Die(); // Initialize the first die
+        dice[1] = new Die(); // Initialize the second die
     }
 
+    // Returns the face value of the die at the specified index
     public int getValue(int index) {
-        return dice[index].getFaceValue();
+        return dice[index].getFaceValue(); // Get the face value of the die at the given index
     }
 
+    // Rolls both dice
     public void roll() {
-        dice[0].roll();
-        dice[1].roll();
+        dice[0].roll(); // Roll the first die
+        dice[1].roll(); // Roll the second die
     }
 
+    // Returns the sum of the face values of both dice
     public int getSum() {
-        return dice[0].getFaceValue() + dice[1].getFaceValue();
+        return dice[0].getFaceValue() + dice[1].getFaceValue(); // Get the sum of both dice
     }
 
+    // Checks if both dice have the same face value
     public boolean isSame() {
-        return dice[0].getFaceValue() == dice[1].getFaceValue();
+        return dice[0].getFaceValue() == dice[1].getFaceValue(); // Return true if both dice show the same value
     }
 }
